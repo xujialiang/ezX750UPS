dd87c0507522387f77ff762addcbf0e639470614
xujialiang <mail@xujialiang.net>
2021-04-28 00:44:59 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # ezX750UPS
+
+## 树莓派配置
+
+```
+sudo raspi-config
+
+```
+
+选择 5 Interfacing Options
+
+选择 I2C - Enable/Disable automatic loading
+
+选择 "Yes" 
+
+
+## 安装
+
+```
+npm install ezx750ups --save
+```
+
+## 使用
+
+```
+const EZX750 = require('ezx750ups');
+let ezX750Mgr = EZX750(0x36);
+
+// 获取电压
+let vol = ezX750Mgr.getVoltage();
+console.log('vol', vol);
+
+
+let cap = ezX750Mgr.getCapacity();
+console.log('cap', cap);
+
+```
+
+有技术问题可微信我
+
+![wechat](./vx.jpeg)
+
---
+++ index.js
@@ -0,0 +1,39 @@
+const struct = require('python-struct');
+const i2c = require('i2c-bus');
+
+class ezx750ups {
+    constructor(address=0x36){
+        this.address = address;
+    }
+
+    getVoltage(busNum=1){
+        let rawData = i2c.openSync(busNum).readWord(this.address, 0x2);
+        let data = this._readVoltage(rawData);
+        return data;
+    }
+
+    getCapacity(busNum=1){
+        let rawData = i2c.openSync(busNum).readWord(this.address, 0x4);
+        let data = this._readVoltage(rawData);
+        return data;
+    }
+
+    _readVoltage(rawData){
+        console.log('rawData', rawData);
+        let packData = struct.pack(">H", rawData);
+        console.log('packData', packData);
+        let unpackData = struct.unpack("<H", packData);
+        console.log('unpackData', unpackData[0]);
+        let vol = unpackData[0]*1.25/1000/16;
+        return vol;
+    }
+
+    _readCapacity(rawData){
+        let packData = struct.pack(">H", rawData);
+        console.log('packData', packData);
+        let unpackData = struct.unpack("<H", packData);
+        console.log('unpackData', unpackData[0]);
+        let cap = unpackData[0]/256;
+        return cap;
+    }
+}
